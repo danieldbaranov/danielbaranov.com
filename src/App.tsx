083a5d1f751e521187f5da0dcc744f2a1d710600
Daniel Baranov <dbaranov@localhost.localdomain>
2025-05-12 22:33:49 -0400

    Updated website to no longer use create-react-app template or dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+let PUBLIC_URL = '../public'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={process.env.PUBLIC_URL + '/DanielHeadshot.jpg'} className="App-logo" alt="logo" />
+        <img src={'/DanielHeadshot.jpg'} className="App-logo" alt="logo" />
         <div>
             <p>
                 The official website of Daniel Baranov.
@@ -13,12 +15,12 @@ function App() {
             <div className={"logo-bar"}>
                 <a href="https://github.com/danieldbaranov">
                     <img
-                        src={process.env.PUBLIC_URL + '/githublogo.png'} alt={"github"}
+                        src={'/githublogo.png'} alt={"github"}
                         className={"portfolio-logo"}
                     />
                 </a>
                 <a href="https://www.linkedin.com/in/danielbaranov">
-                    <img src={process.env.PUBLIC_URL + '/linkedinlogo.png'} alt={"linkedin"}
+                    <img src="/linkedinlogo.png" alt={"linkedin"}
                          className={"portfolio-logo"}/>
                 </a>
             </div>
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    fallback: {
+      process: require.resolve('process/browser'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  mode: 'development',
+};
+
